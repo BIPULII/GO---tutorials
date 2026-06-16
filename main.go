a2c7b3244262f8bf4734a6192af52584cc038a5d
Bipuli <bipuli@Bipulis-MacBook-Air.local>
2026-06-16 09:49:38 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	//fmt.Println("Hello, World!");
+	var a int = 30
+	fmt.Println("Value of a is", a)
+	const b int = 20
+	fmt.Println("Value of b is", b)
+	a = 40
+	fmt.Println("Value of a is", a)
+}
