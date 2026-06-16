df5c1dc0564961ca7d2b89ab90bb562c8953e007
Bipuli <bipuli@Bipulis-MacBook-Air.local>
2026-06-16 10:34:32 +0530

    simple project for ticketbooking

--- main.go
+++ main.go
@@ -4,10 +4,20 @@ import "fmt"
 
 func main() {
 	//fmt.Println("Hello, World!");
-	var a int = 30
-	fmt.Println("Value of a is", a)
-	const b int = 20
-	fmt.Println("Value of b is", b)
-	a = 40
-	fmt.Println("Value of a is", a)
+	
+	var conferenceName string = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v\n", conferenceName)
+	fmt.Println("Get your tickets here to attend the conference.");
+	fmt.Printf("There are %v tickets remaining.\n", remainingTickets)
+
+	fmt.Printf("Enter your first name: ")
+	var firstName string
+	fmt.Scan(&firstName)
+	fmt.Printf("Enter the number of tickets you want to book: ")
+	var numTickets int
+	fmt.Scan(&numTickets)
+	fmt.Printf("Thank you %v for booking %v tickets.\n", firstName, numTickets)
 }
